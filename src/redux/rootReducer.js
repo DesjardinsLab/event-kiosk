5d6125e6f3020482a92a5caadec490f6d0befc1c
Nicolas Lavoie Drapeau <lavoiedn@gmail.com>
2016-01-12 11:06:29 -0500

    Cleaned up some more stuff

--- src/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import { routeReducer as router } from 'redux-simple-router'
-import presentation from './modules/presentation'
-
-export default combineReducers({
-  presentation,
-  router
-})
