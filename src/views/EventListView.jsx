9e232241bbd80fad48d23924915f5b75e5be2885
Nicolas Lavoie Drapeau <lavoiedn@gmail.com>
2016-01-14 16:06:21 -0500

    Added event classification by months

--- src/views/EventListView.jsx
+++ src/views/EventListView.jsx
@@ -14,12 +14,13 @@ export class EventListView extends React.Component {
 
   componentDidMount () {
     var eventsByMonth = {}
+    var monthFormat = new Intl.DateTimeFormat(LOCALE, { month: 'long', year: 'numeric' })
 
     for(var i = 0; i < this.props.events.length; i++) {
       var event = this.props.events[i]
       event.key = i
 
-      var month = new Intl.DateTimeFormat(LOCALE, { month: 'long' }).format(new Date(event.startTime))
+      var month = monthFormat.format(new Date(event.startTime))
 
       if (eventsByMonth[month]) {
         eventsByMonth[month].push(event)
@@ -31,19 +32,42 @@ export class EventListView extends React.Component {
     this.setState({eventsByMonth})
   }
 
+  getEvents (month) {
+    return (
+      this.state.eventsByMonth[month].map(function (event, index) {
+        console.log(event)
+        return (
+          <EventListItem
+            key={event.key}
+            event={event}
+            LOCALE={LOCALE}
+          />
+        )
+      })
+    )
+  }
+
   render () {
+    var listContent = <div />
+
+    if (this.state.eventsByMonth) {
+      listContent = (
+        <div>
+          {Object.keys(this.state.eventsByMonth).map(function (month, index) {
+            return (
+              <div className='eventsByMonth'>
+                <h1 className='month'>{month}</h1>
+                {this.getEvents(month)}
+              </div>
+            )
+          }.bind(this))}
+        </div>
+      )
+    }
 
     return (
       <div className='eventList'>
-          {this.props.events.map(function (item, index) {
-            return (
-              <EventListItem
-                key={index}
-                event={item}
-                LOCALE={LOCALE}
-              />
-            )
-          })}
+          {listContent}
       </div>
     )
   }
