95561f66750c8d63b9f936cf9be06a771e60f6b1
Cedric Veilleux <veilleux.cedric@gmail.com>
2016-01-19 10:56:38 -0500

    Better dockerfile with nginx/gunicorn/django prod

---
+++ backend/run-prod.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate --noinput auth
+python3 manage.py migrate --noinput
+/usr/local/bin/gunicorn ecd.wsgi -w 4 -b 127.0.0.1:5000 --log-level=info --chdir=/app
