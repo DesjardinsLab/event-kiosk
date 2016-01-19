1c353b9c86935732db5cf3c71610ca16287a3f23
Cedric Veilleux <veilleux.cedric@gmail.com>
2016-01-19 17:45:16 -0500

    Added SPEAKERS models and linked to events.

--- backend/event_kiosk/event_kiosk/events/models.py
+++ backend/event_kiosk/event_kiosk/events/models.py
@@ -2,6 +2,22 @@ from django.db import models
 from django.utils.translation import ugettext as _
 from datetime import datetime
 
+class Speaker(models.Model):
+    name = models.CharField(_('name'), max_length=255)
+    image = models.ImageField(_('image'), upload_to='speakers/%Y/%m/%d/', null=True)
+    bio = models.TextField(_('bio'))
+
+    def to_json(self):
+        return {
+            'name': self.name,
+            'image': self.image.url,
+            'bio': self.bio,
+        }
+
+    def __str__(self):
+        return self.name
+
+
 class Event(models.Model):
     title = models.CharField(_('title'), max_length=255)
     date = models.DateField(_('date'), null=False)
@@ -10,16 +26,21 @@ class Event(models.Model):
     description = models.TextField(_('description'))
     image = models.ImageField(_('image'), upload_to='events/%Y/%m/%d/', null=False)
     location = models.CharField(_('location'), max_length=100)
+    speakers = models.ManyToManyField(Speaker)
 
     def to_json(self):
+        speakers = []
+        for speaker in self.speakers.all():
+            speakers.append(speaker.to_json())
+
         return {
-            'type': 'event',
             'title': self.title,
             'startTime': datetime.combine(self.date, self.startTime),
             'endTime': datetime.combine(self.date, self.endTime),
             'desc': self.description,
             'img': self.image.url,
-            'location': self.location
+            'location': self.location,
+            'speakers': speakers
         }
 
     def __str__(self):
