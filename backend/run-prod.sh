a1a340e621cb696ded59bc208d802b22da84ddf8
Cedric Veilleux <veilleux.cedric@gmail.com>
2016-01-19 11:06:41 -0500

    Add gunicorn dep + fix paths

--- backend/run-prod.sh
+++ backend/run-prod.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+cd /app/event_kiosk
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput auth
 python3 manage.py migrate --noinput
-/usr/local/bin/gunicorn ecd.wsgi -w 4 -b 127.0.0.1:5000 --log-level=info --chdir=/app
+/usr/local/bin/gunicorn ecd.wsgi -w 4 -b 127.0.0.1:5000 --log-level=info --chdir=/app/event_kiosk
