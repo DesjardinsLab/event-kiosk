51834d5056ce85b5e133a3f3a3c89031845a2f7b
Nicolas Lavoie Drapeau <lavoiedn@gmail.com>
2016-01-20 13:51:08 -0500

    Started working on kiosk refactor

---
+++ src/views/KioskView.jsx
@@ -0,0 +1,174 @@
+import React from 'react'
+
+import AppBar from 'material-ui/lib/app-bar'
+import LeftNav from 'material-ui/lib/left-nav'
+import MenuItem from 'material-ui/lib/menus/menu-item'
+import Divider from 'material-ui/lib/divider'
+
+import Home from 'material-ui/lib/svg-icons/actions/home'
+
+import LinearProgress from 'material-ui/lib/linear-progress'
+
+import ObjectHash from 'object-hash'
+
+import Isvg from 'react-inlinesvg'
+
+const RELOAD_INTERVAL = 60000
+
+export class PresentationView extends React.Component {
+  constructor (props) {
+    super()
+
+    this.state = {
+      appTitle: '',
+      interactiveMode: false,
+      navOpen: false,
+      presentation: {
+        transitionTime: 0,
+        pauseTimeOnTouch: 60000,
+        slides: []
+      },
+      transitionProgress: 0,
+      allowInput: true
+    }
+  }
+
+  componentDidMount () {
+    this.getKiosk()
+    this.setState({reloadInterval: setInterval(this.getKiosk.bind(this), RELOAD_INTERVAL)})
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.state.reloadInterval)
+  }
+
+  getKiosk () {
+    var url = typeof KIOSK_SOURCE !== 'undefined' ? KIOSK_SOURCE : 'example-data.json'
+
+    fetch(url).then(function (response) {
+      return response.json()
+    }).then(function (content) {
+      // If data changed, reload page.
+      var hashedData = ObjectHash(content, {algorithm: 'md5'})
+      if (this.state.dataHash && hashedData !== this.state.dataHash) {
+        location.reload()
+      }
+      this.setState({ dataHash: hashedData })
+
+      content.presentation.currentSlide = content.presentation.slides[0]
+      content.presentation.currentSlideIndex = 0
+      content.presentation.slides = content.presentation.slides
+
+      this.setState(content)
+    }.bind(this)).catch(function (error) {
+      console.log(error)
+      console.log('Error while fetching data. Will try again in a minute.')
+    })
+  }
+
+  toggleNav (event) {
+    this.setState({ navOpen: !this.state.navOpen })
+  }
+
+  hideAppBar (hide) {
+    this.setState({ hideAppBar: hide })
+  }
+
+  handleInteraction (event) {
+    this.setState({
+      interactiveMode: true
+    })
+  }
+
+  handleInactivityTimer () {
+    this.setState({
+      interactiveMode: false
+    })
+    this.setState({ progressRefreshInterval: setInterval(this.handleTransitionTimer.bind(this), LINEAR_PROGRESS_REFRESH_RATE) })
+  }
+
+  createLeftNav () {
+    <LeftNav
+      open={this.state.navOpen}
+      onRequestChange={navOpen => this.setState({navOpen})}
+      docked={false}
+    >
+      <MenuItem value='home' primaryText="Accueil">
+        <Home />
+      </MenuItem>
+      <Divider />
+      {sections.map(function (section, sectionIndex) {
+        return (
+          <MenuItem
+            key={sectionIndex}
+            disabled={true}
+            primaryText={section.title}/>
+          section.pages.map(function (page, pageIndex) {
+            <MenuItem
+              key={pageIndex}
+              value={sectionIndex +  '-' + pageIndex}>
+              <Isvg src={page.icon} />{page.title}
+            </MenuItem>
+          })
+          <Divider />
+        )
+      })}
+    </LeftNav>
+  }
+
+  // these functions should be passed to children to access components from the kiosk.
+
+  setProgressBarValue (value) {
+    this.setState({ transitionProgress: value })
+  }
+
+  toggleInteractiveMode () {
+    this.setState({ interactiveMode: !this.state.interactiveMode })
+  }
+
+  setAppTitle (title) {
+    this.setState({ apptitle: title })
+  }
+
+  render () {
+    var sections = this.state.sections
+    var progressBarClasses = 'progressBar'
+
+    if (this.state.transitionProgress === 0) {
+      // Allows the removal of the transition from full to empty
+      progressBarClasses += ' empty'
+    }
+
+    return (
+      <div className='kiosk' style={this.state.allowInput ? {} : {pointerEvents: 'none'}} onClick={(event) => this.handleInteraction(event)}>
+        <LinearProgress
+          className={progressBarClasses}
+          mode='determinate'
+          max={this.state.presentation.transitionTime}
+          value={this.state.transitionProgress}
+        />
+        <AppBar
+          className='appBar'
+          title={this.state.appTitle}
+          onLeftIconButtonTouchTap={(event) => this.toggleNav(event)}
+          zDepth={0}
+          style={this.state.hideAppBar ? {
+           opacity: 0,
+           position: 'absolute',
+           pointerEvents: 'none'
+          } : {
+            opacity: 1,
+            pointerEvents: 'auto'
+          }}
+        />
+        {this.createLeftNav()}
+        <PresentationView {...this.props}
+          presentation={this.state.presentation}
+          setProgressBarValue={(value) => this.setProgressBarValue(value)}
+        />
+      </div>
+    )
+  }
+}
+
+export default PresentationView
--- src/views/PresentationView.jsx
+++ src/views/PresentationView.jsx
@@ -20,78 +20,15 @@ const IMAGE_SLIDE_TYPE = 'image'
 const LINEAR_PROGRESS_REFRESH_RATE = 300;
 
 export class PresentationView extends React.Component {
-  constructor (props) {
-    super()
-
-    this.state = {
-      start: null,
-      interactiveMode: false,
-      navOpen: false,
-      presentation: {
-        transitionTime: 0,
-        pauseTimeOnTouch: 60000,
-        slides: []
-      },
-      transitionProgress: 0,
-      allowInput: true
-    }
-  }
-
   componentDidMount () {
-    this.getPresentation()
-    this.setState({reloadInterval: setInterval(this.getPresentation.bind(this), 60000)})
-  }
-
-  componentWillUnmount () {
-    clearTimeout(this.state.activityTimer)
-    clearInterval(this.state.reloadInterval)
-    clearInterval(this.state.progressRefreshInterval)
-  }
-
-  getPresentation () {
-    var url = typeof PRESENTATION_SOURCE !== 'undefined' ? PRESENTATION_SOURCE : 'example-data.json'
-
-    fetch(url).then(function (response) {
-      return response.json()
-    }).then(function (content) {
-      // If data changed, reload page.
-      var hashedData = ObjectHash(content, {algorithm: 'md5'})
-      if (this.state.dataHash && hashedData !== this.state.dataHash) {
-        location.reload()
-      }
-      this.setState({ dataHash: hashedData })
-
-      content.currentSlide = content.presentation.slides[0]
-      content.currentSlideIndex = 0
-      content.slides = content.presentation.slides
-
-      this.setState(content)
-
-      // Start slideshow timer when view is mounted
-      if (this.state.presentation.transitionTime > 0 && !this.state.progressRefreshInterval) {
-        this.setState({ progressRefreshInterval: setInterval(this.handleTransitionTimer.bind(this), LINEAR_PROGRESS_REFRESH_RATE) })
-      }
-    }.bind(this)).catch(function (error) {
-      console.log(error)
-      console.log('Error while fetching data. Will try again in a minute.')
-    })
-  }
-
-  handleClick (event) {
-    if (this.state.allowInput) {
-      if (this.state.activityTimer) { clearTimeout(this.state.activityTimer) }
-      if (this.state.progressRefreshInterval) {
-        clearInterval(this.state.progressRefreshInterval)
-      }
-      this.setState({
-        interactiveMode: true, transitionProgress: 0, start: null,
-        activityTimer: setTimeout(this.handleInactivityTimer.bind(this), this.state.presentation.pauseTimeOnTouch)
-      })
+    // Start slideshow timer when view is mounted
+    if (this.props.transitionTime > 0 && !this.state.progressRefreshInterval) {
+      this.setState({ progressRefreshInterval: setInterval(this.handleTransitionTimer.bind(this), LINEAR_PROGRESS_REFRESH_RATE) })
     }
   }
 
   handleTransitionTimer () {
-    if (!this.state.interactiveMode) {
+    if (!this.props.interactiveMode) {
       if (this.state.transitionProgress + LINEAR_PROGRESS_REFRESH_RATE < this.state.presentation.transitionTime) {
         this.setState({transitionProgress: this.state.transitionProgress ? this.state.transitionProgress + LINEAR_PROGRESS_REFRESH_RATE : LINEAR_PROGRESS_REFRESH_RATE})
       } else { // Reset progress bar to 0 and go to next slide
@@ -106,17 +43,6 @@ export class PresentationView extends React.Component {
         })
       }
     }
-  }
-
-  handleInactivityTimer () {
-    this.setState({
-      interactiveMode: false
-    })
-    this.setState({ progressRefreshInterval: setInterval(this.handleTransitionTimer.bind(this), LINEAR_PROGRESS_REFRESH_RATE) })
-  }
-
-  toggleNav (event) {
-    this.setState({navOpen: !this.state.navOpen})
   }
 
   onSlideChange (index, element) {
@@ -138,9 +64,50 @@ export class PresentationView extends React.Component {
     }
   }
 
-  render () {
-    var slides = this.state.presentation.slides
+  buildSwipeComponent () {
+    return (
+      <div className='kiosk-swiper'>
+        <ReactSwipe
+          continuous={this.props.slides.length > 2}
+          onTouchStart={(event) => this.handleClick(event)}
+          onTouchEnd={(event) => this.handleImageScroll(event)}
+          slideToIndex={this.state.currentSlideIndex}
+          key='react-swipe'
+          callback={(index, element) => this.onSlideChange(index, element)}>
+          {this.props.slides.map(function (item, index) {
+            if (item.type === EVENT_LIST_SLIDE_TYPE) {
+              return (
+                <div key={'eventListWrapper' + index}>
+                  <EventView
+                    {...this.props}
+                    events={item.events}
+                    hideAppBar={(hide) => this.hideAppBar(hide)}
+                  />
+                </div>
+              )
+            } else if (item.type === IMAGE_SLIDE_TYPE) {
+              return (
+                <div key={'imgWrapper' + index} style={{backgroundImage: 'url('+item.img+')', backgroundSize: 'cover', height: '100vh'}}/>
+              )
+            } else if (item.type === EVENT_SLIDE_TYPE) {
+              return (
+                <div key={'eventWrapper' + index}>
+                  <EventView
+                    {...this.props}
+                    event={item.event}
+                    isStatic={true}
+                    hideAppBar={(hide) => this.hideAppBar(hide)}
+                  />
+                </div>
+              )
+            }
+          }.bind(this))}
+        </ReactSwipe>
+      </div>
+    )
+  }
 
+  render () {
     var progressBarClasses = 'progressBar'
 
     if (this.state.transitionProgress === 0) {
@@ -148,94 +115,15 @@ export class PresentationView extends React.Component {
       progressBarClasses += ' empty'
     }
 
+    // if slides have not yet been added to state, render nothing.
     var reactSwipeComponent = <div />
 
-    if (this.state.slides) {
-      reactSwipeComponent = (
-        <div className='kiosk-swiper'>
-          <ReactSwipe
-            continuous={this.state.slides.length > 2}
-            onTouchStart={(event) => this.handleClick(event)}
-            onTouchEnd={(event) => this.handleImageScroll(event)}
-            slideToIndex={this.state.currentSlideIndex}
-            key='react-swipe'
-            callback={(index, element) => this.onSlideChange(index, element)}>
-            {this.state.slides.map(function (item, index) {
-              if (item.type === EVENT_LIST_SLIDE_TYPE) {
-                return (
-                  <div key={'eventListWrapper' + index}>
-                    <EventView
-                      {...this.props}
-                      events={item.events}
-                      monthFormat={this.props.monthFormat}
-                      dateFormat={this.props.dateFormat}
-                      hideAppBar={(hide) => this.hideAppBar(hide)}
-                      timeIntervalFormat={this.props.timeIntervalFormat}
-                      pauseTimeOnTouch={this.state.presentation.pauseTimeOnTouch}
-                    />
-                  </div>
-                )
-              } else if (item.type === IMAGE_SLIDE_TYPE) {
-                return (
-                  <div key={'imgWrapper' + index} style={{backgroundImage: 'url('+item.img+')', backgroundSize: 'cover', height: '100vh'}}/>
-                )
-              } else if (item.type === EVENT_SLIDE_TYPE) {
-                return (
-                  <div key={'eventWrapper' + index}>
-                    <EventView
-                      {...this.props}
-                      event={item.event}
-                      isStatic={true}
-                      hideAppBar={(hide) => this.hideAppBar(hide)}
-                      pauseTimeOnTouch={this.state.presentation.pauseTimeOnTouch}
-                    />
-                  </div>
-                )
-              }
-            }.bind(this))}
-          </ReactSwipe>
-        </div>
-      )
+    if (this.props.slides) {
+      reactSwipeComponent = this.buildSwipeComponent()
     }
 
     return (
-      <div className='kiosk-presentation' style={this.state.allowInput ? {} : {pointerEvents: 'none'}} onClick={(event) => this.handleClick(event)}>
-        <LinearProgress
-          className={progressBarClasses}
-          mode='determinate'
-          max={this.state.presentation.transitionTime}
-          value={this.state.transitionProgress}
-        />
-        <AppBar
-          className='appBar'
-          title={this.state.currentSlide ? this.state.currentSlide.title : ''}
-          onLeftIconButtonTouchTap={(event) => this.toggleNav(event)}
-          zDepth={0}
-          style={this.state.hideAppBar ?
-          {
-            display: 'none'
-          } : (this.state.currentSlide && !this.state.interactiveMode ? this.state.currentSlide.type === IMAGE_SLIDE_TYPE : false) ? {
-           opacity: 0,
-           pointerEvents: 'none'
-          } : {
-            opacity: 1,
-            pointerEvents: 'auto'
-          }}
-        />
-        <LeftNav
-          open={this.state.navOpen}
-          onRequestChange={navOpen => this.setState({navOpen})}
-          docked={false}>
-          {slides.map(function (item, index) {
-            return (
-              <MenuItem key={index} onClick={() => function() {
-                this.setState({currentSlideIndex: index})
-              }}>
-                {item.title}
-              </MenuItem>
-            )
-          })}
-        </LeftNav>
+      <div className='kiosk-presentation' style={this.props.allowInput ? {} : {pointerEvents: 'none'}} onClick={(event) => this.handleClick(event)}>
         {reactSwipeComponent}
       </div>
     )
